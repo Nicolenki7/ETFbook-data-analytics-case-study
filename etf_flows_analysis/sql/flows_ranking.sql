ad11ba5abc22d1f024b9e3a4da19d426bc6507a8
Nicolas Zalazar <zalazarn046@gmail.com>
2025-12-31 18:07:56 +0100

    Remove ranked_flows CTE from flows_ranking.sql
    Removed the ranked_flows CTE from the SQL query.

--- etf_flows_analysis/sql/flows_ranking.sql
+++ etf_flows_analysis/sql/flows_ranking.sql
@@ -15,15 +15,3 @@ SELECT
   ) AS flow_rank
 FROM `etfbook_case_study.etf_flows_2025`
 ORDER BY date, flow_rank;
-
-WITH ranked_flows AS (
-  SELECT
-    date,
-    etf_ticker,
-    net_flows_usd_m,
-    DENSE_RANK() OVER (
-      PARTITION BY date
-      ORDER BY net_flows_usd_m DESC
-    ) AS rank
-  FROM `etfbook_case_study.etf_flows_2025`
-);
